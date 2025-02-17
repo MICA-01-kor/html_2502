1f61117ee45648dd7d0c98250a5e71a266d26ca3
MICA-01-kor <hoyoung2818@gmail.com>
2025-02-17 15:35:42 +0900

    과제

---
+++ ex_0217/html/js/index01.js
@@ -0,0 +1,63 @@
+x = 5;
+y = 6;
+z = x + y;
+//body문서 > 요소의 Id "demo" > html 안쪽 = "~~"
+document.getElementById("demo1").innerHTML = "The value of z is: " + z;
+
+var x1 = 5;
+var y1 = 6;
+var z1 = x1 + y1;
+document.getElementById("demo2").innerHTML = "The value of z1 is :" + z1;
+
+let x2 = 5;
+let y2 = 6;
+let z2 = x2 + y2;
+document.getElementById("demo3").innerHTML = "The value of z2 is :" + z2;
+
+const x3 = 5;
+const y3 = 6;
+const z3 = x3 + y3;
+document.getElementById("demo4").innerHTML = "The value of z3 is :" + z3;
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+document.getElementById("demo5").innerHTML = "The total is :" + total;
+
+const pi = 3.14;
+let person = "John Deo";
+let answer = 'Yes I am!';
+document.getElementById("demo6").innerHTML = pi + "<br>" + person + "<br>" + answer;
+
+let carName = "Volvo";
+document.getElementById("demo7").innerHTML = carName;
+
+let person1 = "John Doe", carName1 = "Audi", price = 200;
+document.getElementById("demo8").innerHTML = carName1;
+
+let carName2;
+document.getElementById("demo9").innerHTML = carName2;
+
+var carName3 = "Audi";
+var caName3;
+document.getElementById("demo10").innerHTML = carName3;
+
+let a = 5 + 2 + 3;
+document.getElementById("demo11").innerHTML = a;
+
+let a1 = "John" + " " + "Doe";
+document.getElementById("demo12").innerHTML = a1;
+
+let a2 = "5" + 3 + 2;
+document.getElementById("demo13").innerHTML = a2;
+
+let a3 = 2 + 3 + "5";
+document.getElementById("demo14").innerHTML = a3;
+
+let $$$ = 2;
+let $myMoney = 5;
+document.getElementById("demo15").innerHTML = $$$ + $myMoney;
+
+let _x = 2;
+let _100 = 5;
+document.getElementById("demo16").innerHTML = _x + _100;
